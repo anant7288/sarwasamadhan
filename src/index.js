887cee3ad464c5e234c5d572e525ee80ea77024e
Anant <vakadu10@gmail.com>
2020-10-10 02:22:51 +0530

    router added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routes from './Routes.js'
 
-ReactDOM.render(<div><h1>Sarwasamadhan is under construction!!!</h1><h5>Coming Soon!!!</h5></div>, document.getElementById('root'));
+ReactDOM.render(
+    <Routes />,
+    document.getElementById('root')
+)
+// ReactDOM.render(<div><h1>Sarwasamadhan is under construction!!!</h1><h3>Coming Soon!!!</h3></div>, document.getElementById('root'));
