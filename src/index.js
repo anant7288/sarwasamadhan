4232de9daea7df2c36417105604b8cd38344df8a
anant7288 <anant.kumar@bridgeweave.com>
2024-10-06 15:50:25 +0530

    as

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import reactDOM from "react-dom";
 import "./styles/style.scss";
-import Routes from './Routes.js'
+import Routes from "./Routes.js";
 
 // const template = React.createElement('h1', null, 'Sarwa Samadhan is Under Construction!!!')
-    
-// 
 
-const App = ()=>{
-    return(
-        <div>
-            <h1>Sarwa Samadhan is Under Construction!!!</h1>
-            <h3>Coming Soon!!!</h3>
-        </div>
-    )
-}
-reactDOM.render(<Routes />,document.getElementById("root"))
+//
+
+const App = () => {
+  return (
+    <div>
+      <h1>Sarwa Samadhan is Under Construction!!!</h1>
+      <h3>Coming Soon</h3>
+    </div>
+  );
+};
+reactDOM.render(<Routes />, document.getElementById("root"));
